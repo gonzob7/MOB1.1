cbca543348aefa39fabee556f034471b9b3bbc69
Gonzalo Birrueta <birruetabusiness@gmail.com>
2019-11-20 16:01:54 -0800

    added Equatable Protocol Example

--- ProtocolExamples.playground/Contents.swift
+++ ProtocolExamples.playground/Contents.swift
@@ -1,9 +1,44 @@
 import Foundation
 
-//Case Iterable Protocal Example
+//Case Iterable Protocol Example
 enum Tree: CaseIterable{
     case redwood
     case pine
 }
 
 print(Tree.allCases.count)
+
+
+
+//Equatable Protocol on Class
+class StreetAddress {
+    let number: String
+    let street: String
+    let unit: String?
+
+    init(_ number: String, _ street: String, unit: String? = nil) {
+        self.number = number
+        self.street = street
+        self.unit = unit
+    }
+}
+
+
+extension StreetAddress: Equatable {
+    static func == (lhs: StreetAddress, rhs: StreetAddress) -> Bool {
+        return
+            lhs.number == rhs.number &&
+            lhs.street == rhs.street &&
+            lhs.unit == rhs.unit
+    }
+}
+
+let addresses = [StreetAddress("1490", "Grove Street"),
+                 StreetAddress("2119", "Maple Avenue"),
+                 StreetAddress("1400", "16th Street")]
+let home = StreetAddress("1400", "16th Street")
+
+print(addresses[0] == home)
+// Prints "false"
+print(addresses.contains(home))
+// Prints "true"
